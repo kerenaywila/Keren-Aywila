25479bbd26e3331587fd8699be84817d3f5baa7d
evereverk <kerensss64@gmail.com>
2024-04-23 08:20:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const navBar= document.querySelector(".navbar");
+
+menuToggle.addEventListener("click", () => {
+  menuToggle.classList.toggle("active");
+  navBar.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-links").forEach((n) =>
+  n.addEventListener("click", () => {
+    menuToggle.classList.remove("active");
+    navBar.classList.remove("active");
+  })
+);
